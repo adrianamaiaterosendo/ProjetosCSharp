7c33d93c2aed1a38ed10d84f9a2c010672f06857
adrianamaiate <66576219+adrianamaiate@users.noreply.github.com>
2020-08-10 20:54:13 -0300

    Adicionando Projetos C# criados no curso da Microsoft

---
+++ ProjetoMetodoMath/AulaMetodoMath/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AulaMetodoMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue = Math.Max(firstValue, secondValue);
+
+            Console.WriteLine("O maior número entre " + firstValue + " e " + secondValue + " é " + largerValue);
+        }
+    }
+}
