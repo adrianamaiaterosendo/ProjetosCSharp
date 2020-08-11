fc6bb149f246b67dcca95415bab91a1724c8cc03
adrianamaiate <66576219+adrianamaiate@users.noreply.github.com>
2020-08-10 21:27:07 -0300

    Incluindo Projeto LancarDado

---
+++ ProjetoLancarDado/ProjetoLancarDado/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjetoLancarDado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+        }
+    }
+}
