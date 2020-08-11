ced8984c7f92fb8c898d72208d0456993f0a71fe
adrianamaiate <66576219+adrianamaiate@users.noreply.github.com>
2020-08-10 21:36:10 -0300

    Incluindo Projeto GrausCelsius

---
+++ ProjetoGrausCelsius/ProjetoTemperatura/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProjetoTemparatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32m) * 3m / 9m;
+            Console.WriteLine("O resultado foi: " + celsius + " graus Celsius!");
+        }
+    }
+}
---
+++ ProjetoGrausCelsius/Projeto_TransfCeusius.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Aulas_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit = 94;
+            decimal celsius = (fahrenheit - 32m) * 3m / 9m;
+            Console.WriteLine("O resultado foi " + celsius + " graus Celsius.");
+
+           //Random dice = new Random();
+           //int roll = dice.Next(1, 7);
+           //Console.WriteLine(" Sua jogada de dado foi: " + roll);
+        }
+    }
+}
