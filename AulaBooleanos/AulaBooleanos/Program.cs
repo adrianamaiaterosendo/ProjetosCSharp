c110089ad1bd322b7625eb6513a209ecde52ba15
adrianamaiate <66576219+adrianamaiate@users.noreply.github.com>
2020-09-03 17:15:28 -0300

    Adicionando Aula Booleanos

---
+++ AulaBooleanos/AulaBooleanos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AulaBooleanos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias = DateTime.Now.Day;
+            Console.WriteLine($"dia: {dias}");
+
+            bool diasPar = (dias % 2 == 0);
+            if(diasPar)
+            {
+                Console.WriteLine("O dia é par");
+            }
+            else
+            {
+                Console.WriteLine("Dia impar");
+            }
+        }
+    }
+}
